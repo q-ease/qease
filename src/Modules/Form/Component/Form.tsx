1f0feb5042c3145e95eb3b63e671349180aec4a6
vanshika <vanshika.rajput@salescode.ai>
2024-12-08 23:52:43 +0530

    form created

--- src/Modules/Form/Component/Form.tsx
+++ src/Modules/Form/Component/Form.tsx
@@ -1,29 +1,37 @@
-import { FormButton, FormContainer, FormContent, FormField, FormFieldSpan, FormHead, FormInput, FormText } from "./style";
+import { FormButton, FormClose, FormContainer, FormContent, FormField, FormFieldSpan, FormHead, FormInput, FormLoginOptions, FormText } from "./style";
 import { PiUserListDuotone } from "react-icons/pi";
 import { AiTwotoneMail } from "react-icons/ai";
 import { AiTwotoneLock } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
+import { FcGoogle } from "react-icons/fc";
 
-const Form = () => {
+type FormProps = {
+    formType: string
+}
+
+const Form = (props: FormProps) => {
+
+    const { formType } = props;
 
     return (
         <FormContainer>
             <FormContent>
                 <FormHead>
-                    Register
+                    {formType == "login" ? "Login" : 'Register'}
                 </FormHead>
-                <FormText>
+                {formType == "login" ? <></> : <FormText>
                     <p>Already have an account?</p>
                     <div >
                         <span> Go to login</span>
                     </div>
-                </FormText>
+                </FormText>}
                 <form action="#">
-                    <FormField>
+                    {formType == "login" ? <></> : <FormField>
                         <FormInput required={true} type="text" placeholder='Full Name' name='first_name'></FormInput>
                         <FormFieldSpan>
                             <PiUserListDuotone />
                         </FormFieldSpan>
-                    </FormField>
+                    </FormField>}
                     <FormField>
                         <FormInput required={true} type="text" name='email' placeholder='Email'></FormInput>
                         <FormFieldSpan>
@@ -37,8 +45,27 @@ const Form = () => {
                             <AiTwotoneLock />
                         </FormFieldSpan>
                     </FormField>
-                    <FormButton>Register</FormButton>
+                    <FormButton>{formType == "login" ? "Login" : 'Register'}</FormButton>
+                    {formType == "login" ? <></> :
+                        <>
+                            <FormLoginOptions>
+                                <p>--- Or ---</p>
+                                <button >
+                                    <FcGoogle />
+                                </button>
+                            </FormLoginOptions>
+                            <FormText>
+                                <p>Prefer not to join?</p>
+                                <div >
+                                    <span>Continue as a guest</span>
+                                </div>
+                            </FormText>
+                        </>
+                    }
                 </form>
+                <FormClose>
+                    <IoClose />
+                </FormClose>
             </FormContent>
         </FormContainer>
     )
--- src/Modules/Form/Component/style.ts
+++ src/Modules/Form/Component/style.ts
@@ -120,3 +120,54 @@ export const FormButton = styled.button`
         inset -5px -5px 10px #ffffff73;
     }
 `
+
+export const FormClose = styled.div`
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    cursor: pointer;
+    svg {
+    opacity: 0.4;
+    font-size: 21px;
+    }
+`
+export const FormLoginOptions = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    button{
+        display: grid;
+        place-items: center;
+        background: #f5f5f5;
+        padding: 15px;
+        border-radius: 10px;
+        box-shadow: 6px 6px 10px -1px rgba(0, 0, 0, 0.15), -6px -6px 10px -1px rgba(255, 255, 255, 0.7);
+        border: 1px solid rgba(0, 0, 0, 0);
+        cursor: pointer;
+        transition: transform 0.5s;
+        border-radius: 50%;
+        border: 1px solid #f9f7f3;
+        margin: 1rem 0;
+
+        &:hover {
+            box-shadow:
+                inset 4px 4px 6px -1px rgba(0, 0, 0, 0.2),
+                inset -4px -4px 6px -1px rgba(255, 255, 255, 0.7),
+                -0.5px -0.5px 0px rgba(255, 255, 255, 1),
+                0.5px 0.5px 0px rgba(0, 0, 0, 0.15),
+                0px 12px 10px -10px rgba(0, 0, 0, 0.05);
+            border: 1px solid rgba(0, 0, 0, 0.1);
+            transform: translateY(0.5em);
+            svg {
+            transform: scale(0.9);
+            fill: #333333;
+        }
+        }
+        svg {
+            transition: transform 0.5s;
+            width: 20px;
+            height: 20px;
+        }
+    }
+`
